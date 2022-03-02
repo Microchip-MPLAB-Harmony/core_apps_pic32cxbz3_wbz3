ac0103589c9027729b550ef19e928757087d4d7c
Arpan Kumar <arpan.kumar@microchip.com>
2022-03-02 22:00:06 +0530

    [SYS_FS] Added nvm fat fs demo
    MH3-63276

---
+++ apps/fs/nvm_fat/firmware/src/config/wbz351_curiosity/initialization.c
@@ -0,0 +1,331 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+/*** FUSERID ***/
+#pragma config USER_ID =      0xffff
+
+/*** DEVCFG0 ***/
+#pragma config SWOEN =      ON
+#pragma config TROEN =      OFF
+#pragma config ADCPOVR =      HW
+#pragma config ACCMP1_ALTEN =      PA13
+#pragma config CPENFILT =      OFF
+#pragma config RTCIN0_ALTEN =      PA3
+#pragma config RTCOUT_ALTEN =      PA4
+#pragma config SMBUSEN0 =      OFF
+#pragma config SMBUSEN1 =      OFF
+#pragma config HPLUGDIS =      OFF
+#pragma config SLRTEN0 =      OFF
+#pragma config SLRTEN1 =      OFF
+#pragma config SLRTEN2 =      OFF
+#pragma config PCM =      SFR
+#pragma config INT0E =      OFF
+#pragma config INT0P =      NEG
+#pragma config FECCCON =         OFF
+#pragma config FRECCDIS =      OFF
+
+
+/*** DEVCFG1 ***/
+#pragma config TRCEN =      ON
+#pragma config ECC_SEL_MEM =      ROWA
+#pragma config ZBTWKSYS =      OFF
+#pragma config CMP0_OE =      OFF
+#pragma config CMP1_OE =      OFF
+#pragma config SLRCTRL0 =      GPIO
+#pragma config SLRCTRL1 =      GPIO
+#pragma config SLRCTRL2 =      GPIO
+#pragma config SMCLR =      NO_POR
+#pragma config QSCHE_EN =      OFF
+#pragma config QSPI_HSEN =      PPS
+#pragma config SCOM0_HSEN =      PPS
+#pragma config SCOM1_HSEN =      PPS
+#pragma config CCL_OE =      ON
+#pragma config I2CDSEL0 =      OFF
+#pragma config I2CDSEL1 =      OFF
+#pragma config I2CDSEL2 =      OFF
+#pragma config WDTPSS =      PSS1048576
+#pragma config QSPIDDRM =      OFF
+#pragma config CLKZBREF =      OFF
+
+/*** DEVCFG2 ***/
+#pragma config ACMP_CYCLE =      _32US
+#pragma config DMTINTV =      WIN_127_128
+#pragma config PMUTEST_VDD_EN =      VDD_DIV_2
+#pragma config POSCMOD =      HS
+#pragma config WDTRMCS =      LPRC
+#pragma config SOSCSEL =      XTAL
+#pragma config WAKE2SPD =      ON
+#pragma config CKSWEN =      ON
+#pragma config FSCMEN =      ON
+#pragma config WDTPSR =      PS1048576
+#pragma config WDTSPGM =      STOPS
+#pragma config WINDIS =      NORMAL
+#pragma config WDTEN =      OFF
+#pragma config WDTWINSZ =    WINSZ_25
+#pragma config DMTCNT =      DMT31
+#pragma config DMTEN =      OFF
+
+
+/*** DEVCFG4 ***/
+#pragma config SOSCCFG =    0x0
+#pragma config RTCEVENT_SEL =      ONE_SEC
+#pragma config RTCEVENT_EN =      OFF
+#pragma config VBKP_1KCSEL =      _32K
+#pragma config VBKP_32KCSEL =      FRC
+#pragma config VBKP_DIVSEL =      DIV_31_25
+#pragma config LPCLK_MOD =      DIV_1
+#pragma config RTCEVTYPE =      OUT
+#pragma config CPEN_DLY =      _8_LPRC
+#pragma config DSZPBOREN =      OFF
+#pragma config DSWDTPS =      DSPS5
+#pragma config DSWDTOSC =    LPRC
+#pragma config DSWDTEN =      OFF
+#pragma config DSEN =    OFF
+#pragma config UVREGROVR =      CONTROLLED
+#pragma config LPOSCEN =      OFF
+#pragma config RTCNTM_CSEL =      RAW
+
+/*** FBCFG0 ***/
+#pragma config BINFOVALID =      VALID
+#pragma config PCSCMODE =      SINGLE
+
+/*** FCPN0 ***/
+#pragma config CP =      DISABLED
+
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_MEMORY Instance 0 Initialization Data">
+
+static uint8_t gDrvMemory0EraseBuffer[NVM_ERASE_BUFFER_SIZE] CACHE_ALIGN;
+
+static DRV_MEMORY_CLIENT_OBJECT gDrvMemory0ClientObject[DRV_MEMORY_CLIENTS_NUMBER_IDX0];
+
+static DRV_MEMORY_BUFFER_OBJECT gDrvMemory0BufferObject[DRV_MEMORY_BUFFER_QUEUE_SIZE_IDX0];
+
+const DRV_MEMORY_DEVICE_INTERFACE drvMemory0DeviceAPI = {
+    .Open               = DRV_NVM_Open,
+    .Close              = DRV_NVM_Close,
+    .Status             = DRV_NVM_Status,
+    .SectorErase        = DRV_NVM_SectorErase,
+    .Read               = DRV_NVM_Read,
+    .PageWrite          = DRV_NVM_PageWrite,
+    .EventHandlerSet    = (DRV_MEMORY_DEVICE_EVENT_HANDLER_SET)DRV_NVM_EventHandlerSet,
+    .GeometryGet        = (DRV_MEMORY_DEVICE_GEOMETRY_GET)DRV_NVM_GeometryGet,
+    .TransferStatusGet  = (DRV_MEMORY_DEVICE_TRANSFER_STATUS_GET)DRV_NVM_TransferStatusGet
+};
+
+const DRV_MEMORY_INIT drvMemory0InitData =
+{
+    .memDevIndex                = 0,
+    .memoryDevice               = &drvMemory0DeviceAPI,
+    .isMemDevInterruptEnabled   = true,
+    .isFsEnabled                = true,
+    .deviceMediaType            = (uint8_t)SYS_FS_MEDIA_TYPE_NVM,
+    .ewBuffer                   = &gDrvMemory0EraseBuffer[0],
+    .clientObjPool              = (uintptr_t)&gDrvMemory0ClientObject[0],
+    .bufferObj                  = (uintptr_t)&gDrvMemory0BufferObject[0],
+    .queueSize                  = DRV_MEMORY_BUFFER_QUEUE_SIZE_IDX0,
+    .nClientsMax                = DRV_MEMORY_CLIENTS_NUMBER_IDX0
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="File System Initialization Data">
+
+
+const SYS_FS_MEDIA_MOUNT_DATA sysfsMountTable[SYS_FS_VOLUME_NUMBER] =
+{
+    {NULL}
+};
+
+const SYS_FS_FUNCTIONS FatFsFunctions =
+{
+    .mount             = FATFS_mount,
+    .unmount           = FATFS_unmount,
+    .open              = FATFS_open,
+    .read              = FATFS_read,
+    .close             = FATFS_close,
+    .seek              = FATFS_lseek,
+    .fstat             = FATFS_stat,
+    .getlabel          = FATFS_getlabel,
+    .currWD            = FATFS_getcwd,
+    .getstrn           = FATFS_gets,
+    .openDir           = FATFS_opendir,
+    .readDir           = FATFS_readdir,
+    .closeDir          = FATFS_closedir,
+    .chdir             = FATFS_chdir,
+    .chdrive           = FATFS_chdrive,
+    .write             = FATFS_write,
+    .tell              = FATFS_tell,
+    .eof               = FATFS_eof,
+    .size              = FATFS_size,
+    .mkdir             = FATFS_mkdir,
+    .remove            = FATFS_unlink,
+    .setlabel          = FATFS_setlabel,
+    .truncate          = FATFS_truncate,
+    .chmode            = FATFS_chmod,
+    .chtime            = FATFS_utime,
+    .rename            = FATFS_rename,
+    .sync              = FATFS_sync,
+    .putchr            = FATFS_putc,
+    .putstrn           = FATFS_puts,
+    .formattedprint    = FATFS_printf,
+    .testerror         = FATFS_error,
+    .formatDisk        = (FORMAT_DISK)FATFS_mkfs,
+    .partitionDisk     = FATFS_fdisk,
+    .getCluster        = FATFS_getclusters
+};
+
+
+
+const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
+{
+    {
+        .nativeFileSystemType = FAT,
+        .nativeFileSystemFunctions = &FatFsFunctions
+    },
+};
+
+
+// </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+
+  
+    CLK_Initialize();
+    /* Configure Prefetch, Wait States */
+    PCHE_REGS->PCHE_CHECON = (PCHE_REGS->PCHE_CHECON & (~(PCHE_CHECON_PFMWS_Msk | PCHE_CHECON_ADRWS_Msk | PCHE_CHECON_PREFEN_Msk))) 
+                                    | (PCHE_CHECON_PFMWS(1) | PCHE_CHECON_PREFEN(1));
+
+
+	GPIO_Initialize();
+
+    EVSYS_Initialize();
+
+    NVM_Initialize();
+
+	BSP_Initialize();
+
+    sysObj.drvMemory0 = DRV_MEMORY_Initialize((SYS_MODULE_INDEX)DRV_MEMORY_INDEX_0, (SYS_MODULE_INIT *)&drvMemory0InitData);
+
+
+
+    /*** File System Service Initialization Code ***/
+    SYS_FS_Initialize( (const void *) sysFSInit );
+
+
+    APP_Initialize();
+
+
+    NVIC_Initialize();
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
